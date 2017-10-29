74de1209cbe309f80994b0b11f9a0a88fd9f6e27
caminale loic <caminalel@gmail.com>
2017-10-29 17:42:54 +0100

    update, and delete user in function of id

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,22 @@ export default Route.extend({
   actions: {
     createUser(name) {
       const user = this.get('store').createRecord('user', {name});
-      //console.log(`luser ${user}`);
 
       user.save();
+    },
+    deleteUser(id) {
+        this.store.findRecord('user', id ,{ backgroundReload: false }).then(function(user) {
+        user.deleteRecord();
+        user.get('isDeleted'); // => true
+        user.save(); // => DELETE to /users/1
+      });
+    },
+    updateUser(id,userName) {
+      console.log(userName+id);
+      this.get('store').findRecord('user',id).then(function (user) {
+        user.set('name',userName);
+        user.save();
+      })
     }
-  }
+    }
 });
