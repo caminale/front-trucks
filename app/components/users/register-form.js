aad75e80169f77640ac269f2274a336be4795910
caminale loic <caminalel@gmail.com>
2017-11-01 18:41:03 +0100

    connection to register done, need to put condition on user already created

--- app/components/users/register-form.js
+++ app/components/users/register-form.js
@@ -1,12 +1,18 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
+  session: Ember.inject.service('session'),
+
   identification: '',
   password: '',
   actions: {
     register(){
-      console.log('yollooo'+password);
       this.sendAction('register', this.get('identification'),this.get('password'));
+      let { identification, password } = this.getProperties('identification', 'password');
+      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+        this.set('errorMessage', reason.error || reason);
+      });
     }
   }
 });
