614e3bb6ec8eb3565ae6e62ad22605bb722daae5
caminale loic <caminalel@gmail.com>
2017-11-03 00:10:17 +0100

    manage doublouns during registering

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,10 +1,26 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+  session: Ember.inject.service('session'),
+
   actions: {
     register(name, password) {
       const user = this.get('store').createRecord('user', {name:name, password:password});
-      user.save();
-    }
+// we make an request to server to know if the user exist
+      const request = $.ajax({
+        url:' http://localhost:8080/availableUser',
+        type:'POST',
+        data: {name: name, password: password}
+      });
+      request.then(function(results){
+        console.log(results);
+//if it doesn't exist we can creat it
+        if(results.result ==='okUserNotExisting'){
+          user.save();
+        }
+      });
+    },
+
   }
 });
