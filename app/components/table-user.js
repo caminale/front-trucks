62226c4196df28d406fb1e71001dd2cea88b51ff
jadedagher <jadedagher@Jades-MacBook-Pro.local>
2017-11-11 15:15:21 +0100

    try to create semantic-ui design to display our DB data in table page

--- app/components/table-user.js
+++ app/components/table-user.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { Table } from 'ember-semantic-ui-table'
+import Columns from '../mixins/columns';
+import layout from '../templates/components/tables/table-user';
+import Ember from 'ember';
 
-export default Component.extend({
+export default Component.extend(Columns, {
+  layout,
+  table: Ember.computed('columns', 'model', function() {
+    return new Table(this.get('columns'), this.get('model'));
+  })
 });
---
+++ app/mixins/columns.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  columns: Ember.computed(function() {
+    return [
+      {
+        cellType: 'row-number',
+        verticalAlign: 'middle'
+      },
+      {
+        label: 'name',
+        valuePath: 'name',
+        verticalAlign: 'middle'
+      }
+    ];
+  })
+});
